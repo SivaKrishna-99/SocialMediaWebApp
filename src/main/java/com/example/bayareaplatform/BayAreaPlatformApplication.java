2767a3ddc31cfff7387c05190b879187dfaea3bb
Siva Krishna Vattikonda <sivakrishna@vt.edu>
2022-12-20 21:54:30 -0500

    Initial commit

---
+++ src/main/java/com/example/bayareaplatform/BayAreaPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.bayareaplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayAreaPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BayAreaPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bayareaplatform/BayAreaPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bayareaplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BayAreaPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
